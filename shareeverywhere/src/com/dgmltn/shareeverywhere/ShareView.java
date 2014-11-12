e8b4449f075261a150658fdb4cb2d311b5c42566
Evan Tatarka <evan.tatarka@willowtreeapps.com>
2014-11-12 11:07:53 -0500

    Revert "Mavenize, add ABS dependency."
    This reverts commit 415cc919674eabf11cb62d756ea9ccb74d5651b6.

--- shareeverywhere/src/com/dgmltn/shareeverywhere/ShareView.java
+++ shareeverywhere/src/com/dgmltn/shareeverywhere/ShareView.java
@@ -28,6 +28,7 @@ import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
@@ -39,7 +40,6 @@ import android.widget.ImageView;
 import android.widget.PopupWindow;
 import android.widget.TextView;
 
-import com.actionbarsherlock.view.MenuItem;
 import com.dgmltn.shareeverywhere.ActivityChooserModel.ActivityChooserModelClient;
 import com.dgmltn.shareeverywhere.ActivityChooserModel.ActivityResolveInfo;
 
